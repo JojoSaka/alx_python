843f805e8a0218b8018948420f71f83b3d65cc8c
Jojo Saka <144123071+JojoSaka@users.noreply.github.com>
2023-12-04 20:01:48 +0000

    Update 3-last_digit.py

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -1,4 +1,14 @@
 import random
 number = random.randint(-10000, 10000)
 number = str(number)
-print(number[-1])
+print(number)
+new_number= number[-1]
+print(new_number)
+new_number = int(new_number)
+print ("Last digit of {} is {}".format(number, new_number))
+if new_number > 5:
+  print("and is greater than 5")
+elif new_number == 0:
+  print("and is 0")
+elif new_number < 6 and new_number != 0:
+  print("and is less than 6 and not 0")
