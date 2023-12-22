4461f59cbf705790864da8fdd80a4eee5285f153
Jojo Saka <144123071+JojoSaka@users.noreply.github.com>
2023-12-22 14:25:09 +0000

    Update 3-string.py

--- python-functions/3-string.py
+++ python-functions/3-string.py
@@ -1,1 +1,15 @@
-
+def fibonacci_sequence(n):
+    n_1 = 0
+    n_2 = 1
+    count = 0
+    n_terms = int(n)  - 2
+    fina_list = [0, 1]
+        
+    while count < n_terms:
+        ntotal = n_1 + n_2
+        n_1 = n_2
+        n_2 = ntotal
+        fina_list.append(ntotal)
+        count += 1
+        
+    return (fina_list)
